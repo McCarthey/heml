32fd12df8d76a704a116554c2ee0f5c96a738bc6
Dean <mccarthey123@outlook.com>
2018-08-28 16:43:18 +0800

    整合打包、复制脚本

--- build_email.sh
+++ build_email.sh
@@ -1,1 +1,5 @@
-heml build email_MIA_female.heml && heml build email_MIA_male.heml && heml build email_NY_male.heml && heml build email_NY_female.heml
+heml build email_MIA_female.heml && heml build email_MIA_male.heml && heml build email_NY_male.heml && heml build email_NY_female.heml && 
+cp email_MIA_female.html /d/onluxy/static/email_template/ &&
+cp email_MIA_male.html /d/onluxy/static/email_template/ &&
+cp email_NY_female.html /d/onluxy/static/email_template/ &&
+cp email_NY_male.html /d/onluxy/static/email_template/ 

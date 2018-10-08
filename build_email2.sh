55b200b459ff58dd071379a8ab713be6f16ee96f
Dean <mccarthey123@outlook.com>
2018-10-08 11:48:59 +0800

    生日邮件

--- build_email2.sh
+++ build_email2.sh
@@ -1,6 +1,4 @@
 # build 4 new emails for luxy (welcome, message, match)  
-heml build email_welcome.heml && heml build email_single_message.heml && heml build email_more_messages.heml && heml build email_match.heml && 
-cp email_welcome.html /d/onluxy/static/email_template/ &&
-cp email_single_message.html /d/onluxy/static/email_template/ &&
-cp email_more_messages.html /d/onluxy/static/email_template/ &&
-cp email_match.html /d/onluxy/static/email_template/ 
+heml build email_birthdayVIP.heml && heml build email_birthday_no_VIP.heml && heml build email_more_messages.heml && heml build email_match.heml && 
+cp email_birthdayVIP.html /d/onluxy/static/email_template/ &&
+cp email_birthday_no_VIP.html /d/onluxy/static/email_template/

7d6226b30a49d5ab0521e3255f2a785f30288c6e
Dean <mccarthey123@outlook.com>
2018-10-08 15:38:17 +0800

    更新模板结构：banner图

--- build_email2.sh
+++ build_email2.sh
@@ -1,4 +1,4 @@
-# build 4 new emails for luxy (welcome, message, match)  
+# build birthday emails for luxy 
 heml build email_birthdayVIP.heml && heml build email_birthday_no_VIP.heml && heml build email_more_messages.heml && heml build email_match.heml && 
 cp email_birthdayVIP.html /d/onluxy/static/email_template/ &&
 cp email_birthday_no_VIP.html /d/onluxy/static/email_template/

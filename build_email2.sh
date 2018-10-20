d332a9b8c954c64b132085f2d03177552cb1d1cd
Dean <mccarthey123@outlook.com>
2018-10-20 13:32:22 +0800

    email更新

--- build_email2.sh
+++ build_email2.sh
@@ -1,4 +1,4 @@
 # build birthday emails for luxy 
-heml build email_black_expired.heml && heml build email_buy_black_success.heml && 
-cp email_black_expired.html /d/onluxy/static/email_template/ &&
-cp email_buy_black_success.html /d/onluxy/static/email_template/
+heml build email_birthdayVIP.heml && heml build email_birthday_no_VIP.heml && 
+cp email_birthdayVIP.html /d/onluxy/static/email_template/ &&
+cp email_birthday_no_VIP.html /d/onluxy/static/email_template/
